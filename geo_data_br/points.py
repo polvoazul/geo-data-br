5fe35ba07127bf836866bb98a4aa7fa0eb9d91e2
Fred Israel <fredpublico@gmail.com>
2019-12-15 01:19:38 -0300

    fixed empty points error

--- geo_data_br/points.py
+++ geo_data_br/points.py
@@ -4,8 +4,11 @@ import geopandas as gpd
 import pandas as pd
 
 def data_on_points(points: List[Point], level='best'):
+    '''points is a list of longitute, latitude tuples shapely.geometry.Point objects'''
     if level not in ('best', 'all'):
         raise TypeError('choose a suported_level')
+    if not points:
+        return pd.DataFrame()
     points = [Point(p) for p in points]
     points =  gpd.GeoDataFrame(geometry=points)
     points.crs = {'init' :'epsg:4326'} # default to this projection
@@ -13,7 +16,10 @@ def data_on_points(points: List[Point], level='best'):
     udh_level = gpd.sjoin(points, geo_dfs['udh'])
     missing_points = points.index.difference(udh_level.index)
     if level == 'best':
-        municipality_level = gpd.sjoin(points.loc[missing_points], geo_dfs['municipality'])
+        if missing_points.empty:
+            municipality_level = gpd.GeoDataFrame()
+        else:
+            municipality_level = gpd.sjoin(points.loc[missing_points], geo_dfs['municipality'])
         out = pd.concat([udh_level, municipality_level], sort=False)
         assert out.index.shape == points.index.shape
         return out.loc[points.index]
@@ -29,7 +35,6 @@ def get_geo_dataframes():
             path('geo_data_br.data', 'udh.pickle') as udh:
         out['municipality'] = joblib.load(municipality)
         out['udh'] = joblib.load(udh)
-
     for level_name, df in out.items():
         df['level'] = level_name
         df.columns = [c.lower() for c in df.columns]
--- geo_data_br/test/test_feature1.py
+++ geo_data_br/test/test_feature1.py
@@ -6,3 +6,13 @@ class TestDataLake:
         points = [(-59.23352, -3.35030), (-60.17875, -3.27442)]
         df = geo_data_br.points.data_on_points(points)
         assert df.shape[0] == len(points)
+
+        points = [(-60.17875, -3.27442)]
+        df = geo_data_br.points.data_on_points(points)
+        points = [(-59.23352, -3.35030)]
+        df = geo_data_br.points.data_on_points(points)
+
+    def test_empty(self):
+        points = []
+        df = geo_data_br.points.data_on_points(points)
+        assert df.empty
