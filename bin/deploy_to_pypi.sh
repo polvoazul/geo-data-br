48c7277c27fc3695d3c50335a7a0c968e9e97127
Fred Israel <fredpublico@gmail.com>
2020-01-27 13:49:11 -0300

    downloading data from github

--- bin/deploy_to_pypi.sh
+++ bin/deploy_to_pypi.sh
@@ -4,6 +4,8 @@ set -e
 
 ./bin/test.sh
 
+rm -rf dist
 python setup.py sdist bdist_wheel
 twine upload --repository-url https://test.pypi.org/legacy/ dist/*
-# twine upload dist/*
+echo 'to prod? (press enter)'; read;
+twine upload dist/*
--- geo_data_br/points.py
+++ geo_data_br/points.py
@@ -2,6 +2,8 @@ from typing import List
 from shapely.geometry import Point
 import geopandas as gpd
 import pandas as pd
+from appdirs import user_cache_dir
+import os.path
 
 def data_on_points(points: List[Point], level='best'):
     '''points is a list of longitute, latitude tuples or shapely.geometry.Point objects'''
@@ -26,21 +28,46 @@ def data_on_points(points: List[Point], level='best'):
     elif level == 'all':
         raise NotImplementedError
 
-from importlib.resources import path
-from zipfile import ZipFile
+_cache_dir = user_cache_dir('geo_data_br', 'polvoazul')
+_inea_filepath = os.path.join(_cache_dir, 'inea_data.zip')
+
 import functools, joblib
 @functools.lru_cache()
 def get_geo_dataframes():
     out = {}
-    with path('geo_data_br.data', 'data.zip') as data_path, \
-            ZipFile(data_path, 'r') as data, \
-            data.open('municipality.pickle') as municipality, \
-            data.open('udh.pickle') as udh:
-        out['municipality'] = joblib.load(municipality)
-        out['udh'] = joblib.load(udh)
+    if not os.path.isfile(_inea_filepath):
+        _download_data()
+    full_path = lambda p: os.path.join(_cache_dir, p)
+    out['municipality'] = joblib.load(full_path('municipality.pickle'))
+    out['udh'] = joblib.load(full_path('udh.pickle'))
     for level_name, df in out.items():
         df['level'] = level_name
         df.columns = [c.lower() for c in df.columns]
         df.to_crs({'init': 'epsg:4326'}, inplace=True)
         df.sindex
     return out
+
+def _download_data():
+    from urllib.request import urlretrieve
+    from zipfile import ZipFile
+    import re
+    try:
+        from importlib.metadata import version
+    except ImportError: # py < 3.8
+        from importlib_metadata import version
+
+    lib_version = version('geo_data_br')
+    if re.search(r'[a-zA-Z]', lib_version):
+        lib_version = 'master'
+    url = f'https://github.com/polvoazul/geo-data-br/blob/{lib_version}/geo_data_br/data/data.zip?raw=true'
+    os.makedirs(_cache_dir, exist_ok=True)
+    try:
+        urlretrieve(url, _inea_filepath)
+        with ZipFile(_inea_filepath, 'r') as data:
+            data.extractall(_cache_dir)
+    except: # intentional raw except
+        try: os.unlink(_inea_filepath)
+        except FileNotFoundError: pass
+        raise
+
+
